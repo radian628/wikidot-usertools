ae1faee969d54f1400c0e4a0b9f7fa21f9a5b126
radian628 <70352880+radian628@users.noreply.github.com>
2025-09-29 14:54:33 -0700

    fix css unfucker

--- build/unfuck-all-css/unfuck-all-css.user.js
+++ build/unfuck-all-css/unfuck-all-css.user.js
@@ -1,2 +1,13 @@
 "use strict";(()=>{window.addEventListener("load",()=>{setTimeout(()=>{let o=Array.from(document.querySelectorAll("style"));for(let e of o)e.parentElement.removeChild(e);setTimeout(()=>{for(let e of o)e.textContent+="/*force refresh lol*/",document.head.appendChild(e)},200),console.log("hi")},100)});})();
+/*!
+// ==UserScript==
+// @name        Unfuck All CSS 
+// @namespace   Violentmonkey Scripts
+// @grant       none
+// @match *://*.wikidot.com/*
+// @version     1.0
+// @author      radian628
+// @description 9/13/2025, 12:35:21 PM
+// ==/UserScript==
+*/
 //# sourceMappingURL=unfuck-all-css.user.js.map
--- src/unfuck-all-css/unfuck-all-css.user.ts
+++ src/unfuck-all-css/unfuck-all-css.user.ts
@@ -1,3 +1,15 @@
+/*!
+// ==UserScript==
+// @name        Unfuck All CSS 
+// @namespace   Violentmonkey Scripts
+// @grant       none
+// @match *://*.wikidot.com/*
+// @version     1.0
+// @author      radian628
+// @description 9/13/2025, 12:35:21 PM
+// ==/UserScript==
+*/
+
 window.addEventListener("load", () => {
   setTimeout(() => {
     const styles = Array.from(document.querySelectorAll("style"));
